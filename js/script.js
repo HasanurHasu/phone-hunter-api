cf11704eee6ba57094a2badb43bc4b1b0e77b538
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-08-29 22:27:10 +0600

    added modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const phoneLoad = async (searchText, isShowAll) => {
+const phoneLoad = async (searchText = "samsung", isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -6,7 +6,7 @@ const phoneLoad = async (searchText, isShowAll) => {
 }
 
 const displayPhones = (phones, isShowAll) => {
-    console.log(phones);
+    // console.log(phones);
 
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.innerHTML = "";
@@ -18,7 +18,7 @@ const displayPhones = (phones, isShowAll) => {
         showAllBtn.classList.add('hidden');
     }
 
-    if(!isShowAll){
+    if (!isShowAll) {
         phones = phones.slice(0, 9);
     }
 
@@ -33,7 +33,7 @@ const displayPhones = (phones, isShowAll) => {
         <h2 class="card-title">${phone.phone_name}</h2>
         <p class="my-4 text-lg">There are many variations of passages of available, but the majority have suffered</p>
         <div class="card-actions">
-            <button class="btn btn-primary py-2 px-6 text-white">Show Details</button>
+            <button onclick="handleShowDetails('${phone.slug}')"  class="btn btn-primary py-2 px-6 text-white">Show Details</button>
         </div>
         </div>  
         `;
@@ -60,9 +60,46 @@ const loadingDots = (isLoading) => {
     }
 }
 
-const handleShowAll = () =>{
+const handleShowAll = () => {
     handleSearch(true);
 }
 
+const handleShowDetails = async (id) => {
+    
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phoneData = data.data;
+    console.log(phoneData);
 
-// phoneLoad();
+
+    showPhoneDetails(phoneData);
+}
+
+const showPhoneDetails = (phoneData) => {
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML = `
+    <div class="flex flex-col items-center">
+    <img src="${phoneData.image}" alt="">
+    </div>
+    <h3 class="font-bold text-lg">${phoneData.name}</h3>
+     <p class="py-1 text-sm text-left">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+     <p class="py-1 text-[#706F6F] text-sm text-left"><span class="text-black font-semibold">Storage :</span> ${phoneData.mainFeatures?.storage || 'No Storage'}</p>
+     <p class="py-1 text-[#706F6F] text-sm text-left"><span class="text-black font-semibold">Display Size :</span> ${phoneData.mainFeatures?.displaySize}</p>
+     
+     <p class="py-1 text-[#706F6F] text-sm text-left"><span class="text-black font-semibold">Chipset :</span> ${phoneData.mainFeatures?.chipSet}</p>
+     <p class="py-1 text-[#706F6F] text-sm text-left"><span class="text-black font-semibold">Memory :</span> ${phoneData.mainFeatures?.memory}</p>
+     <p class="py-1 text-[#706F6F] text-sm text-left"><span class="text-black font-semibold">Brand :</span> ${phoneData.brand}</p>
+
+     <div class="modal-action">
+         <!-- if there is a button in form, it will close the modal -->
+         <button class="btn">Close</button>
+     </div>
+    
+    `
+
+
+
+    my_details_modal.showModal()
+}
+
+phoneLoad();
